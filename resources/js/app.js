0feb23a32b14db301159c0426b51ceec9934cb52
kyawlinntun-gif <kyawlinntun100@gmail.com>
2022-04-27 00:26:14 +0630

    2. Setting up Vue.js application, 3. Fallback route in Laravel, 4. Installing and setting up Vue Router, and 5. HTML5 history mode in Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import router from './routes';
+
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
